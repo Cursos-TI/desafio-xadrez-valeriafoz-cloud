d9f4521b44d7f9cc135f66ee9e34cba0c0310d34
valeriafoz-cloud <valeriafoz@gmail.com>
2026-06-02 00:15:57 +0000

    Nível Mestre Completo

--- newxadrez.c
+++ newxadrez.c
@@ -2,42 +2,56 @@
 
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
+// Definição de função de movimentos por recursividade:
+void torre(int casas1) {
+    if (casas1 > 0){
+        printf("Direita\n");
+        torre(casas1 - 1);
+    }
+}
+void bispo(int casas2) {
+    if (casas2 > 0){
+        printf("Cima\n");
+        printf("Direita\n");
+        bispo(casas2 - 1);
+    }
+}
+
+void rainha(int casas3) {
+    if (casas3 > 0){
+        printf("Esquerda\n");
+        rainha(casas3 - 1);
+    }
+}
 
 int main(){
 // Movimentação do Bispo = cinco casas na diagonal para cima e à direita.
-
-    for(int bispo = 0; bispo < 5; bispo++){
-        printf("Direita\n Cima\n");
-    }
-printf("\n");
+    printf("Bispo:\n");
+    bispo(5);
+    printf("\n");
 
 // Movimentação da Torre = cinco casas para a direita.
-int torre = 0;
-while (torre < 5){
-        printf("Direita\n");
-        torre++;
-    }
-printf("\n");
+    printf("Torre:\n");
+    torre(5);
+    printf("\n");
+
 // Movimentação da Rainha = oito casas para a esquerda.
-int rainha = 0;
-   
-    do {
-        printf("Direita\n");
-        rainha++;
-    } while (rainha < 8);
+    printf("Rainha:\n");   
+    rainha(8);
+    printf("\n");
 
-printf("\n");
-
-// Movimentação do Cavalo = duas para baixo, uma para a esquerda.
+// Movimentação do Cavalo = duas casas para cima e uma para a direita.
 
 int cavalo = 1;
+
+printf("Cavalo:\n");
 
 while (cavalo--)
 {
     for (int cavalo2 = 0; cavalo2 < 2; cavalo2++){
-        printf("Baixo\n");
+        printf("Cima\n");
     }
-    printf("Esquerda\n");
+    printf("Direita\n");
 }
 
 
